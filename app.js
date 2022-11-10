e7bf4743403b353f02c67a4a40b7983fafcacea8
Mike's Play <zerpamiqueas2019@gmail.com>
2022-11-10 10:54:51 -0300

    filtrar partidos

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 grupo_activo=0
 
 // generamos array de botones
-
 coleccionGrupos=document.getElementById('menu-grupos').getElementsByTagName('li')
 
 // agrego en un loop los eventlisteners a todos los botones
@@ -11,10 +10,22 @@ for (let i = 0; i < 8; i++) {
 }
 
 // defini activarGrupo
-
 function activarGrupo(i){
-
     coleccionGrupos[grupo_activo].classList.toggle('active')
     coleccionGrupos[i].classList.toggle('active')
     grupo_activo=i
+}
+
+//array de grupos
+grupos=["A", "B", "C", "D", "E", "F", "G", "H"]
+
+//funcion para filtrar partidos de grupos
+function mostrarPartidos(i){
+    partidosFiltrados=fixture.filter(chequear_grupo)
+    function chequear_grupo(partido){
+        if (partido.group==grupos[i]){
+            return true
+        }
+    }
+    return partidosFiltrados
 }
